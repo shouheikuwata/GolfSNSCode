a6c0febf2a4110c97f9b04efe664703ee605ecd9
ShoheiKuwata <shouhei.k224k@keio.jp>
2021-04-24 18:07:31 +0900

    Initial Commit

---
+++ GolfProject-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GolfProject-3
+//
+//  Created by 桑田翔平 on 2021/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
